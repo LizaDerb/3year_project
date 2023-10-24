77d794e3950b93cab3bc30eb558db7ffd6e3e7ea
ayugrishanova <90269945+ayugrishanova@users.noreply.github.com>
2023-10-24 15:21:25 +0300

    Update search_functions.py

--- backend/search_functions.py
+++ backend/search_functions.py
@@ -4,7 +4,7 @@ def find_from_lemma(lemma, cur, bigram=False, trigram=False, previous_token=0):
     else:
         bigram_command = ''
     if trigram:
-        trigram_command = f""" AND corpus_tokens.token_id = {previous_token+2}"""
+        trigram_command = f""" AND corpus_tokens.token_id = {previous_token+1}"""
     else:
         trigram_command = ''
     command = f"""
