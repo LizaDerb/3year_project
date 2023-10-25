0f061e764903d81c80526f970308d58584ccd858
ayugrishanova <90269945+ayugrishanova@users.noreply.github.com>
2023-10-26 02:37:02 +0300

    Delete flask_app directory

--- flask_app/backend_functions/tagset.py
+++
@@ -1,5 +0,0 @@
-TAGSET = ['NOUN', 'ADJF', 'ADJS', 'COMP',
-          'VERB', 'INFN', 'PRTF', 'PRTS',
-          'GRND', 'NUMR', 'ADVB', 'NPRO',
-          'PRED', 'PREP', 'CONJ', 'PRCL',
-          'INTJ']
